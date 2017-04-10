82d0c658a8b2b8c752c3888554b899433bd422f5
lifeichao <lifeichao@qmtv.com>
2017-04-10 18:04:50 +0800

    添加网易短信验证码的功能

--- app/auth/views.py
+++ app/auth/views.py
@@ -6,10 +6,63 @@ from . import auth
 from ..models import User, Car, ESResponse
 from .. import db
 from flask_tokenauth import TokenAuth, TokenManager
+import urllib2, urllib
+import time
+import random
+import hashlib
 
 secret_key = 'shanghaienshengqichefuwuyouxiangongsi'
 token_auth = TokenAuth(secret_key=secret_key)
 token_manager = TokenManager(secret_key=secret_key)
+
+netEasyAppkey = '92bf851a021b70338c382258f3025984'
+netEasyAppSecret = '019bdb177012'
+
+#获取验证码
+@auth.route('/getVerifyCode')
+def getVerifyCode():
+
+
+    url = 'https://api.netease.im/sms/sendcode.action'
+    params = {'mobile':'18321165691'}
+    params_urlencode = urllib.urlencode(params)
+
+    req = urllib2.Request(url=url, data=params_urlencode)
+    req.add_header('AppKey', netEasyAppkey)
+    curTime = str(time.time())
+    req.add_header('CurTime', curTime)
+    nonce = str(random.uniform(1000, 9999))
+    req.add_header('Nonce', nonce)
+    checkSum = hashlib.sha1(netEasyAppSecret + nonce + curTime).hexdigest()
+    req.add_header('CheckSum', checkSum)
+    req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+    #{"code":200,"msg":"1","obj":"6560"}
+    print(res)
+    return "OK"
+
+#校验验证码
+@auth.route('/confirmVerifyCode')
+def confirmVerifyCode():
+    url = 'https://api.netease.im/sms/verifycode.action'
+    params = {'mobile':'18321165691', 'code':'6560'}
+    params_urlencode = urllib.urlencode(params)
+
+    req = urllib2.Request(url=url, data=params_urlencode)
+    req.add_header('AppKey', netEasyAppkey)
+    curTime = str(time.time())
+    req.add_header('CurTime', curTime)
+    nonce = str(random.uniform(1000, 9999))
+    req.add_header('Nonce', nonce)
+    checkSum = hashlib.sha1(netEasyAppSecret + nonce + curTime).hexdigest()
+    req.add_header('CheckSum', checkSum)
+    req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+
+    return "OK"
 
 @auth.route('/register', methods=['POST'])
 def register():
@@ -21,7 +74,7 @@ def register():
     password2 = dict.get('password2')
     phone = dict.get('phone')
     #判断用户是否已经注册过了
-    user = User.query.filter_by(name=name).first()
+    user = User.query.filter_by(phone=phone).first()
     if user is not None:
         return ESResponse(code=400, errorMsg="您已经注册过了, 请登录!").to_json()
 
@@ -33,15 +86,15 @@ def register():
 
 @auth.route('/log_in', methods=['GET', 'POST'])
 def log_in():
-    name = request.form['name']
+    phone = request.form['phone']
     password = request.form['password']
 
-    user = User.query.filter_by(name=name, password=password).first()
+    user = User.query.filter_by(phone=phone, password=password).first()
     if user is None:
         return ESResponse(code=400, errorMsg="用户名或密码错误").to_json()
     else:
         userDict = user.modelToDict()
-        token = token_manager.generate(name)
+        token = token_manager.generate(phone)
 
     tokenDict = {"token":token}
     return ESResponse(code=200, data=tokenDict).to_json()
@@ -56,7 +109,7 @@ def log_out():
 @token_auth.token_required
 def myInfo():
     user = g.current_user
-    userDict = {"name":g.current_user}
+    userDict = {"phone":g.current_user}
     return ESResponse(code=200, data=userDict).to_json()
 
 @auth.route('/addCar', methods=['POST'])
@@ -71,12 +124,10 @@ def addCar():
 
 @token_auth.verify_token
 def verify_token(token):
-    name = token_manager.verify(token)
-
-    user = User.query.filter_by(name=name).first()
-    if name is None:
-        return ESResponse(code=400, errorMsg="用户不存在")
-    else:
-        g.current_user = name
-        return 'hi you are' + name
-    return None
+    phone = token_manager.verify(token)
+    user = User.query.filter_by(phone=phone).first()
+    if user is not None:
+        print(user.phone)
+        g.current_user = phone
+        return True
+    return False
